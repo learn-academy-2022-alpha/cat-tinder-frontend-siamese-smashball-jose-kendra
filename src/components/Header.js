83791e07fa232b0df8df0df70a1eb980a895226c
josesison1 <learnacademy@LEARNs-MacBook-Air.local>
2022-04-05 14:20:51 -0700

    completed front end structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ class Header extends Component {
   render() {
     return (
       <>
-        <div class="header"><img src="https://e7.pngegg.com/pngimages/735/708/png-clipart-super-smash-bros-for-nintendo-3ds-and-wii-u-logo-nintendo-super-smash-bros-for-nintendo-3ds-and-wii-u-text.png" height="150px" width="200px" />
-          <div></div></div>
+        <div class="header"><img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/338b54e1-96a0-4815-9d79-016cee4c3fdf/dcgrwzp-b1bf4c40-b600-4127-b0a6-eeea4b788ae8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMzOGI1NGUxLTk2YTAtNDgxNS05ZDc5LTAxNmNlZTRjM2ZkZlwvZGNncnd6cC1iMWJmNGM0MC1iNjAwLTQxMjctYjBhNi1lZWVhNGI3ODhhZTgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.wYL75RZr-5OwPERUe_hClpIGtr-rZ1WP1fWnwzXcTgQ" height="160px" width="600px" />
+        </div>
       </>
     )
   }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -7,7 +7,8 @@ class NotFound extends Component {
   render() {
     return (
       <>
-        <div class="notfound"><p>Not Found</p></div>
+        <div class="notfound"><p>Page Not Found</p>
+          <div><img src="https://pngset.com/images/mario-coin-mario-question-block-text-number-symbol-toy-transparent-png-1193937.png" background-color="transparent" height="350px" width="450px" /></div></div>
       </>
     )
   }
